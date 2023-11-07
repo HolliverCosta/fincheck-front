c508279745a6a008901c5d3c21dbc1f82dfbc4e4
HolliverCosta <holliver.costa@ccc.ufcg.edu.br>
2023-11-07 18:36:11 -0300

    feat: create page transactions

--- src/view/pages/Dashboard/components/Accounts/AccountCard.tsx
+++ src/view/pages/Dashboard/components/Accounts/AccountCard.tsx
@@ -1,5 +1,7 @@
-import { formatCurrency } from "../../../../app/utils/formatCurrency";
-import { BankAccountTypeIcon } from "../../../components/icons/BankAccountTypeIcon";
+import { cn } from "../../../../../app/utils/cn";
+import { formatCurrency } from "../../../../../app/utils/formatCurrency";
+import { BankAccountTypeIcon } from "../../../../components/icons/BankAccountTypeIcon";
+import { useDashboard } from "../DashboardContext/useDashboard";
 
 interface AccountCardProps {
   color: string;
@@ -8,6 +10,8 @@ interface AccountCardProps {
 }
 
 export function AccountCard({ color, name, balance }: AccountCardProps) {
+  const { areValuesVisible } = useDashboard();
+
   return (
     <div
       className="p-4 bg-white rounded-2xl h-[200px] 
@@ -22,7 +26,7 @@ export function AccountCard({ color, name, balance }: AccountCardProps) {
 
       </div>
       <div>
-        <span className="text-gray-800 font-medium tracking-[-0.5px] block">
+        <span className={cn("text-gray-800 font-medium tracking-[-0.5px] block", !areValuesVisible && 'blur-sm')}>
           {formatCurrency(balance)}
         </span>
         <small className="text-gray-600 text-sm">
---
+++ src/view/pages/Dashboard/components/Transactions/useTransactionsController.ts
@@ -0,0 +1,11 @@
+import { useDashboard } from "../DashboardContext/useDashboard";
+
+export function useTransactionsController(){
+  const { areValuesVisible } = useDashboard();
+  return { 
+    areValuesVisible,
+    isInitialLoading: false,
+    isLoading: false,
+    transactions: [],
+  }
+}
